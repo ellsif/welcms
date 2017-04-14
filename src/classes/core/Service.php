fa69ce8a95938c7b12d7368b4c9750c991073382
sakai <hatasu@ellsif.com>
2017-04-14 10:53:01 +0900

    Actionへのパラメータをハッシュに変更

--- src/classes/core/Service.php
+++ src/classes/core/Service.php
@@ -2,39 +2,36 @@
 
 namespace ellsif\WelCMS;
 
-
-use ellsif\Form;
-
 /**
  * Serviceの基底クラス。
  */
 abstract class Service
 {
 
-  public function __construct()
-  {
+    public function __construct()
+    {
 
-  }
+    }
 
-  /**
-   * ページの名前を取得。
-   * SomeNamePage -> someName
-   *
-   * @return string
-   */
-  protected function getName() :string
-  {
-    $class = get_class($this);
-    $class = substr($class, strrpos($class, '\\') + 1, -4);
-    return strtolower((substr($class, 0, 1))) . substr($class, 1);
-  }
+    /**
+     * ページの名前を取得する。
+     * SomeNamePage -> someName
+     *
+     * @return string
+     */
+    protected function getName() :string
+    {
+        $class = get_class($this);
+        $class = substr($class, strrpos($class, '\\') + 1, -4);
+        return strtolower((substr($class, 0, 1))) . substr($class, 1);
+    }
 
-  protected function requireHelpers()
-  {
-    $config = Pocket::getInstance();
-    require_once $config->dirSystem() . '/functions/helper.php';
-    require_once $config->dirSystem() . '/classes/core/Template.php';
-    require_once $config->dirSystem() . '/classes/core/HtmlTemplate.php';
-    require_once $config->dirView() . '/admin/helper.php';
-  }
+    protected function requireHelpers()
+    {
+        $config = Pocket::getInstance();
+        require_once $config->dirSystem() . '/functions/helper.php';
+        require_once $config->dirSystem() . '/classes/core/Template.php';
+        require_once $config->dirSystem() . '/classes/core/HtmlTemplate.php';
+        require_once $config->dirView() . '/admin/helper.php';
+    }
 }
