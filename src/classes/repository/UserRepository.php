b782037cce4a61e5710cf1f233c5b04edba86597
sakai <hatasu@ellsif.com>
2017-05-29 14:06:56 +0900

    ユーザーID、Emailの重複チェック

--- src/classes/repository/UserRepository.php
+++ src/classes/repository/UserRepository.php
@@ -4,64 +4,78 @@ namespace ellsif\WelCMS;
 
 class UserRepository extends Repository
 {
-    /**
-     * カラムの定義
-     *
-     * ## 説明
-     * リポジトリへの初回アクセス時にテーブルが存在しない場合、
-     * 自動的にテーブルが作成されます。
-     */
-    protected $columns = [
-        'userId' => [
-            'label'       => 'ユーザーID',
-            'type'        => 'string',
-            'description' => 'ログインに利用するidです。後からの変更は出来ません。',
-            'validation'  => [
-                ['rule' => 'required'],
-                ['rule' => 'unique'],
-            ],
-        ],
-        'password' => [
-            'label'       => 'パスワード',
-            'type'        => 'string',
-            'description' => '半角英数記号を入力してください。',
-            'onSave'      => '',  // 登録更新時に自動的にハッシュ化する？
-            'validation'  => [
-                ['rule' => 'required'],
-            ],
-        ],
-        'name' => [
-            'label'       => '名前',
-            'type'        => 'string',
-            'description' => '表示用の名前です。後から変更できます。',
-            'validation'  => [
-                ['rule' => 'required'],
-            ],
-        ],
-        'email' => [
-            'label'       => 'メールアドレス',
-            'type'        => 'string',
-            'description' => 'メールアドレスです。idの代わりにログインに利用できます。',
-            'validation'  => [
-                ['rule' => 'required'],
-                ['rule' => 'unique'],
-                ['rule' => 'email'],
-            ],
-        ],
-        'info' => [
-            'label'       => 'ユーザー情報',
-            'type'        => 'string',
-            'onSave'      => '\json_encode',
-            'onLoad'      => '\ellsif\WelCMS\Repository::json_decode',
-            'description' => '任意のユーザー情報をJSON形式で保存します。',
-        ],
-        'token' => [
-            'label'       => 'APIトークン',
-            'type'        => 'string',
-            'description' => 'API呼び出し時に利用するトークンです。',
-        ]
-    ];
 
+    public function __construct($name)
+    {
+        /**
+         * カラムの定義
+         *
+         * ## 説明
+         * リポジトリへの初回アクセス時にテーブルが存在しない場合、
+         * 自動的にテーブルが作成されます。
+         */
+        $this->columns = [
+            'userId' => [
+                'label'       => 'ユーザーID',
+                'type'        => 'string',
+                'description' => 'ログインに利用するidです。後からの変更は出来ません。',
+                'validation'  => [
+                    ['rule' => 'required'],
+                    'function' => function($field, $value, array $params, array $fields) {
+                        $id = $fields['id'] ?? null;
+                        return $this->validateUniqueLoginId($value, $id);
+                    },
+                ],
+            ],
+            'password' => [
+                'label'       => 'パスワード',
+                'type'        => 'string',
+                'description' => '半角英数記号を入力してください。',
+                'onSave'      => '',  // 登録更新時に自動的にハッシュ化する？
+                'validation'  => [
+                    ['rule' => 'required'],
+                ],
+            ],
+            'name' => [
+                'label'       => '名前',
+                'type'        => 'string',
+                'description' => '表示用の名前です。後から変更できます。',
+                'validation'  => [
+                    ['rule' => 'required'],
+                ],
+            ],
+            'email' => [
+                'label'       => 'メールアドレス',
+                'type'        => 'string',
+                'description' => 'メールアドレスです。idの代わりにログインに利用できます。',
+                'validation'  => [
+                    ['rule' => 'required'],
+                    [
+                        'rule' => 'uniqueManagerEmail',
+                        'function' => function($field, $value, array $params, array $fields) {
+                            $id = $fields['id'] ?? null;
+                            return $this->validateUniqueManagerEmail($value, $id);
+                        },
+                        'message' => 'is already in use',
+                    ],
+                    ['rule' => 'email'],
+                ],
+            ],
+            'info' => [
+                'label'       => 'ユーザー情報',
+                'type'        => 'string',
+                'onSave'      => '\json_encode',
+                'onLoad'      => '\ellsif\WelCMS\Repository::json_decode',
+                'description' => '任意のユーザー情報をJSON形式で保存します。',
+            ],
+            'token' => [
+                'label'       => 'APIトークン',
+                'type'        => 'string',
+                'description' => 'API呼び出し時に利用するトークンです。',
+            ]
+        ];
+        parent::__construct($name);
+    }
 
     /**
      * usersテーブルからデータを取得する。
@@ -91,6 +105,22 @@ class UserRepository extends Repository
             $userIds = explode('|', trim($userIds, '|'));
         }
         return $this->list(['id' => $userIds], 'id ASC');
+    }
+
+    protected function validateUniqueLoginId($value, $id)
+    {
+        $userId = $value ?? '';
+        $userRepo = WelUtil::getRepository('User');
+        $users = $userRepo->list(['userId' => $value]);
+        return count($users) == 0 || $users[0]['id'] == $id;
+    }
+
+    protected function validateUniqueManagerEmail($value, $id)
+    {
+        $email = $value ?? '';
+        $userRepo = WelUtil::getRepository('User');
+        $users = $userRepo->list(['email' => $email]);
+        return count($users) == 0 || $users[0]['id'] == $id;
     }
 
 }
