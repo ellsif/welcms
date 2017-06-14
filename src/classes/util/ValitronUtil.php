5f66fde7bde2efd20ecf4b957dc5fb4d9e7ca49e
sakai <hatasu@ellsif.com>
2017-06-14 14:00:58 +0900

    エラー修正

--- src/classes/util/ValitronUtil.php
+++ src/classes/util/ValitronUtil.php
@@ -54,30 +54,31 @@ class ValitronUtil
         }
         foreach ($validations as $name => $_validations) {
             foreach ($_validations as $_validation) {
-                if (isset($_validation['rule'])) {
-                    $rule = $_validation['rule'];
-                    $func = $_validation['function'];
-                    $message = $_validation['message'] ?? null;
-                    if (!$message) {
-                        $message = ValitronUtil::MESSAGES[$rule] ?? null;
+                if (!isset($_validation['rule'])) {
+                    continue;
+                }
+                $rule = $_validation['rule'];
+                $func = $_validation['function'] ?? null;
+                $message = $_validation['message'] ?? null;
+                if (!$message) {
+                    $message = ValitronUtil::MESSAGES[$rule] ?? null;
+                }
+                if (isset($_validation['function'])) {
+                    if (WelUtil::isClosure($func)) {
+                        Validator::addRule(
+                            $rule,
+                            $func,
+                            $message
+                        );
                     }
-                    if (isset($_validation['function'])) {
-                        if (WelUtil::isClosure($func)) {
-                            Validator::addRule(
-                                $rule,
-                                $func,
-                                $message
-                            );
-                        }
-                    }
-                    if (isset($_validation['option'])) {
-                        $_rule = $validator->rule($rule, $name, $_validation['option']);
-                    } else {
-                        $_rule = $validator->rule($rule, $name);
-                    }
-                    if ($message) {
-                        $_rule->message($message);
-                    }
+                }
+                if (isset($_validation['option'])) {
+                    $_rule = $validator->rule($rule, $name, $_validation['option']);
+                } else {
+                    $_rule = $validator->rule($rule, $name);
+                }
+                if ($message) {
+                    $_rule->message($message);
                 }
             }
         }
