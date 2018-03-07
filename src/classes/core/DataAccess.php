df677370b34f99c34b44a14d8be97161d05f2fee
sakai <hatasu@ellsif.com>
2018-03-07 11:07:40 +0900

    countの処理を修正

--- src/classes/core/DataAccess.php
+++ src/classes/core/DataAccess.php
@@ -94,16 +94,20 @@ abstract class DataAccess
     /**
      * 件数を取得する。
      */
-    public function countQuery(string $query, array $params = []): int
+    public function countQuery(string $sql, array $options = []): int
     {
-        $stmt = $this->pdo->prepare($query);
-        foreach($params as $key => $val) {
-            if (is_string($key) && substr($key, 0, 1) !== ':') {
-                $key = ':' . $key;
+        $stmt = $this->pdo->prepare($sql);
+        if (!$options || key(array_slice($options, 0, 1, true)) === 0) {
+            $params = $options;
+        } else {
+            $params = [];
+            foreach($options as $key => $val) {
+                if (is_string($key) && substr($key, 0, 1) !== ':') {
+                    $params[':' . $key] = $val;
+                }
             }
-            $stmt->bindValue($key, $val);
         }
-        if ($stmt->execute()) {
+        if ($stmt->execute($params)) {
             return intval($stmt->fetchColumn());
         } else {
             welLog('error', "DataAccess", "データの取得に失敗しました。エラーコード：" . $stmt->errorCode());
